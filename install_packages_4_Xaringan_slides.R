bde256adac8346ae956cd2103daac15f784d173f
alexsanchezpla <asanchez@ub.edu>
2019-12-16 08:13:54 +0100

    Actualitzacio general

---
+++ install_packages_4_Xaringan_slides.R
@@ -0,0 +1,15 @@
+# Package installation
+arm_from_cran <- c("flexdashboard", "learnr", "bookdown",
+                   "officer", "rticles", "webshot",
+                   "tidyverse", "remotes", "babynames", "magick")
+install.packages(arm_from_cran, dependencies = TRUE)
+arm_from_gh <- c('yihui/xaringan', 'rstudio/blogdown',
+                 'rstudio-education/armcompanion', 
+                 'haozhu233/kableExtra', 'apreshill/bakeoff',
+                 'hebrewseniorlife/memor')
+remotes::install_github(arm_from_gh, dependencies = TRUE)
+webshot::install_phantomjs()
+
+is_installed <- function(pkg_name){
+  setNames(is.element(pkg_name, installed.packages()[, "Package"]), pkg_name)
+} 
